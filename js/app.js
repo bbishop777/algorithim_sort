302c6577e62c654f4afeacf2dde95de6b3873db6
BBishop <brad.bishop68@gmail.com>
2015-12-30 11:05:20 -1000

    Updates files tracked by git

--- js/app.js
+++
@@ -1,9 +0,0 @@
-var bubbleModule = require ('./bubble_sort.js');
-var quickSortModule = require('./quick_sort.js');
-var mergerSortModule = require('./merge_sort.js');
-
-var bubble = bubbleModule;
-var quickSort = quickSortModule;
-var mergeSort = mergerSortModule;
-
-console.log(mergeSort([4, 19, 5, 1]), "mergeSort");
